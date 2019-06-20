c26c3cd8a6f9f9252ebdfe18e40a83725893e4d3
Molly Hopper <mollyhopper95@gmail.com>
2019-06-20 09:06:13 -0400

    presentation

--- js/main.js
+++ js/main.js
@@ -2,3 +2,7 @@ function flip(letter) {
     // document.getElementById(letter).toggleAttribute('flipped');
     $('#'+letter).toggleClass('flipped');
 }
+
+function burger() {
+    document.querySelector('.navbar-menu').classList.toggle('is-active');
+}
